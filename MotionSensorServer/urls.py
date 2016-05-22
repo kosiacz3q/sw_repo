6275fafb72147be42cb3095adda311024727b03c
kosiacz3q <kosiak.lukasz@gmail.com>
2016-05-23 01:37:26 +0200

    Sensor reading post method added

--- MotionSensorServer/urls.py
+++ MotionSensorServer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^motion/reading/([0-9]{4})/([0-1])/$', views.reading),
 ]
--- MotionSensorServer/views.py
+++ MotionSensorServer/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.views.decorators.http import require_http_methods
+from MotionSensorServer.models import Sensor, SensorReading
+import datetime
 
 
 def index(request):
-    return render_to_response("MotionSensorServer/index.html", {}, RequestContext(request))
+	return render_to_response("MotionSensorServer/index.html", {}, RequestContext(request))
+
+
+@require_http_methods(["POST"])
+def reading(request, sensor_id, is_motion_detected):
+	sensor = Sensor.objects.get(pk=sensor_id)
+
+	sensor_reading = SensorReading(
+		sensor=sensor,
+		date=datetime.datetime.now().time(),
+		detected=(is_motion_detected == 1))
+
+	sensor_reading.save()
+
+	pass
