99d3d070061d936bae0b533ca8cd9c495c5f545f
kosiacz3q <kosiak.lukasz@gmail.com>
2016-05-23 01:17:31 +0200

    Models added

--- MotionSensorServer/models.py
+++ MotionSensorServer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Sensor(models.Model):
+	name = models.TextField()
+	activated = models.BooleanField()
+
+
+class SensorReading(models.Model):
+	sensor = models.ForeignKey(Sensor, null=False)
+	date = models.DateTimeField()
+	detected = models.BooleanField(default=False)
