ba2d2ccbcf5f6f061e6017ed9d2bae0295318dd1
Yamadads <spawlak@yahoo.com>
2016-05-23 23:09:17 +0200

    problem with deleting sensors

--- MotionSensorServer/urls.py
+++ MotionSensorServer/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^motion/reading/([0-9]{4})/([0-1])/$', views.reading),
     url(r'^motion/new_sensor/', views.new_sensor, name='new_sensor'),
+    url(r'^motion/sensors/(?P<sensor_id>[0-9]+)/remove/', views.remove_sensor, name='remove_sensor'),
     url(r'^motion/sensors/', views.get_sensors, name='sensors'),
+    url(r'^motion/detections/', views.get_detections, name='detections'),
     url(r'^motion/detections/', views.get_detections, name='detections')
 ]
--- MotionSensorServer/views.py
+++ MotionSensorServer/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.decorators.http import require_http_methods
-from MotionSensorServer.models import Sensor, SensorReading
+from MotionSensorServer.models import Sensor, SensorReading, SensorForm, UserSensor
 import datetime
-from .models import SensorForm, UserSensor
+from django.core.urlresolvers import reverse
 
 
 def index(request):
@@ -59,3 +59,10 @@ def get_detections(request):
     return render_to_response('MotionSensorServer/detections.html', {
         "items": detections
     }, RequestContext(request))
+
+
+@login_required
+def remove_sensor(request, sensor_id):
+    sensor_r = get_object_or_404(Sensor, sensor__pk=sensor_id)
+    sensor_r.delete()
+    return HttpResponseRedirect(reverse('MotionSensorServer:sensors'))
