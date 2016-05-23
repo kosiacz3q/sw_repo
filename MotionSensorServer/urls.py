327490ebb0207f2a434e1b15a11fdb3b6480415c
Yamadads <spawlak@yahoo.com>
2016-05-23 20:37:30 +0200

    buttons on menu

--- MotionSensorServer/urls.py
+++ MotionSensorServer/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^motion/reading/([0-9]{4})/([0-1])/$', views.reading),
-    url(r'^motion/new_sensor/', views.new_sensor)
+    url(r'^motion/new_sensor/', views.new_sensor, name='new_sensor')
 ]
